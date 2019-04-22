8462911327c973fb7cf5270ad70a7d373f5ffe97
eddylin2015 <eddylin2015@gmail.com>
2019-04-22 09:00:08 +0800

    add read xls

--- TesOO.cs
+++ TesOO.cs
@@ -9,6 +9,65 @@ namespace esOO
 {
     public class TesOO
     {
+        public static void readxls()
+        {
+            string filepath = @"d:\tmp_npoi.xls";
+
+            HSSFWorkbook hssfwb;
+
+            using (FileStream file = new FileStream(filepath, FileMode.Open, FileAccess.Read))
+            {
+                hssfwb = new HSSFWorkbook(file);
+            }
+
+            ISheet sheet = hssfwb.GetSheetAt(0);
+           
+            for (int row = 0; row <= sheet.LastRowNum; row++)
+            {
+                if (sheet.GetRow(row) != null) //null is when the row only contains empty cells 
+                {
+                    // Set new cell value
+                    //sheet.GetRow(row).GetCell(0).SetCellValue("foo");
+                    // Console.WriteLine("Row {0} = {1}", row, sheet.GetRow(row).GetCell(0).StringCellValue);
+                    //https://stackoverflow.com/questions/5855813/how-to-read-file-using-npoi
+                    for (int col=0;col< sheet.GetRow(row).Cells.Count; col++)
+                    {
+                        var cell = sheet.GetRow(row).GetCell(col);
+
+
+                        if (cell != null)
+                            switch (cell.CellType)
+                            {
+                                case CellType.Numeric:
+                                    Console.Write(cell.NumericCellValue);
+      
+                                    break;
+                                case CellType.String:
+                                    Console.Write(cell.StringCellValue);
+                                    break;
+                                case CellType.Blank:
+                                    Console.Write(string.Empty);
+                                   
+                                    break;
+                                case CellType.Formula:
+                                    Console.Write(cell.NumericCellValue);
+                                    break;
+                            }
+                        Console.Write("\t");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            
+            // Save the file
+           // using (FileStream file = new FileStream(filepath, FileMode.Open, FileAccess.Write))
+            //{
+           //     hssfwb.Write(file);
+           // }
+
+            Console.ReadLine();
+
+        }
         public static  void doit()
         {
             // Open Template
